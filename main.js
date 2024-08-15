24fec85061d59e866af0bc623b83e7d21e7e50d4
DiogoSilvaDK <diogodasilvasp@hotmail.com>
2024-08-15 12:15:14 -0300

    Add aterações de ico e windows

--- main.js
+++ main.js
@@ -1,13 +1,27 @@
 // console.log("Processo principal")
-const { app, BrowserWindow } = require("electron")
+const { app, BrowserWindow, nativeTheme } = require("electron")
+const { close } = require("original-fs")
 
 const createWindow = () => {
+    nativeTheme.themeSource = "dark"
     const win = new BrowserWindow({
         width: 800,
-        height: 600
+        height: 600,
+        icon: './src/public/img/abobora.png',
+        // resizable: false
+        // autoHideMenuBar: true
+        // titleBarStyle: 'hidden'
     })
     win.loadFile('./src/views/index.html')
 }
 app.whenReady().then(() => {
     createWindow()
+
+    app.on("activate", () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
 })
