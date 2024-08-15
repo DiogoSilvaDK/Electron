57140bb69a1dbecf4e6c7fcba037c722e573aa1e
DiogoSilvaDK <diogodasilvasp@hotmail.com>
2024-08-15 11:40:21 -0300

    Add interface primaria

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log("Processo principal")
+// console.log("Processo principal")
+const { app, BrowserWindow } = require("electron")
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600
+    })
+    win.loadFile('./src/views/index.html')
+}
+app.whenReady().then(() => {
+    createWindow()
+})
