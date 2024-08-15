58943ed503a95cddea281126ec082901058aa380
DiogoSilvaDK <diogodasilvasp@hotmail.com>
2024-08-15 13:20:18 -0300

    Add menu new

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // console.log("Processo principal")
-const { app, BrowserWindow, nativeTheme } = require("electron")
+const { app, BrowserWindow, nativeTheme, Menu } = require("electron")
 const { close } = require("original-fs")
 
 
@@ -10,21 +10,24 @@ const createWindow = () => {
         width: 800,
         height: 600,
         icon: './src/public/img/abobora.png',
+
         // resizable: false
         // autoHideMenuBar: true
         // titleBarStyle: 'hidden'
     })
+    // menu personalizado
+    Menu.setApplicationMenu(Menu.buildFromTemplate(templete))
     win.loadFile('./src/views/index.html')
 }
 
 // janela sobre
 const aboutWindow = () => {
     const about = new BrowserWindow({
-        width:360,
-        height:220,
+        width: 360,
+        height: 220,
         icon: './src/public/img/abobora.png',
-        autoHideMenuBar:true,
-        resizable:false
+        autoHideMenuBar: true,
+        resizable: false
     })
 
     about.loadFile('./src/views/sobre.html')
@@ -40,3 +43,34 @@ app.whenReady().then(() => {
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') app.quit()
 })
+
+
+// templete menu 
+const templete = [
+    {
+        label: 'Arquivo',
+        submenu: [
+            {
+                label: 'Sair',
+                click: () => app.quit(),
+                accelerator: 'Alt+F4'
+            }
+        ]
+    },
+    {
+        label: 'Exibir',
+        submenu: [
+            {
+                label: 'Recarregar',
+                role: 'reload'
+            },
+            {
+                label: 'Ferramentas do desenvolvedor',
+                role: 'toggleDevTools'
+            }
+        ]
+    },
+    {
+        label: 'Ajuda'
+    }
+]
